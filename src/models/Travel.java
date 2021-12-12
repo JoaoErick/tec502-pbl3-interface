1d26e5c0f55cd44ec6e1a46d76928b43091f659d
AllanCapistrano <allan.capistrano3@gmail.com>
2021-12-12 16:18:23 -0300

    Fix identation.

--- src/models/Travel.java
+++ src/models/Travel.java
@@ -6,15 +6,16 @@ import java.util.List;
 
 /**
  * Trajeto entre a cidade de origem e a cidade de destino.
- * 
+ *
  * @author Allan Capistrano
  * @author João Erick Barbosa
  */
 public class Travel implements Serializable {
+
     /*-------------------------- Constante -----------------------------------*/
     private static final float BASE_VALUE = 3200;
     /*------------------------------------------------------------------------*/
-    
+
     private LinkedList<List<Edge>> route;
     private int totalParts;
     private String cities;
@@ -28,67 +29,68 @@ public class Travel implements Serializable {
     public Travel() {
         this.route = new LinkedList<>();
     }
-    
+
     /**
-     * Inicializa atributos: totalParts, cities, totalCompanyTariff, totalPrice 
+     * Inicializa atributos: totalParts, cities, totalCompanyTariff, totalPrice
      * e totalTime.
      */
-    public void setAttributes(){
-        this.totalParts = this.route.size()-1;
+    public void setAttributes() {
+        this.totalParts = this.route.size() - 1;
         formatCities();
         calculate();
     }
-    
+
     /**
      * Formata o atributo cities para exibição.
      */
-    private void formatCities(){
+    private void formatCities() {
         this.cities = "";
         for (int j = 0; j < this.route.size(); j++) {
             this.cities += this.route.get(j).get(0).getFirstCity().getCityName() + " -> ";
-            if(j == this.route.size() - 1){
+            if (j == this.route.size() - 1) {
                 this.cities += this.route.get(j).get(0).getSecondCity().getCityName();
             }
         }
     }
-    
+
     /**
-     * Calcula o valor total da tarifa da viagem, tempo total de voo e o 
-     * preço total.
+     * Calcula o valor total da tarifa da viagem, tempo total de voo e o preço
+     * total.
      */
-    private void calculate(){
+    private void calculate() {
         this.totalCompanyTariff = 0;
         this.totalTime = 0;
-        
+
         for (int j = 0; j < this.route.size(); j++) {
             this.totalTime += this.route.get(j).get(0).getTimeTravel();
             this.totalCompanyTariff += this.route.get(j).get(0).getCompanyTariff();
+            System.out.println("[" + j + "]" + totalCompanyTariff);
         }
-        
+
         this.totalPrice = (float) (BASE_VALUE / this.totalTime) + this.totalCompanyTariff;
     }
-    
+
     /**
-     * Calcula o valor total da tarifa da viagem, tempo total de voo e o 
-     * preço total através de opções de trechos específicas.
-     * 
+     * Calcula o valor total da tarifa da viagem, tempo total de voo e o preço
+     * total através de opções de trechos específicas.
+     *
      * @param indexes List<Integer> - Lista de trechos específicos.
      */
-    public void calculate(List<String> indexes){
+    public void calculate(List<String> indexes) {
         this.totalCompanyTariff = 0;
         this.totalTime = 0;
         for (int j = 0; j < this.route.size(); j++) {
             for (int i = 0; i < this.route.get(j).size(); i++) {
-                if(this.route.get(j).get(i).getCompanyName().equals(indexes.get(j))){
+                if (this.route.get(j).get(i).getCompanyName().equals(indexes.get(j))) {
                     this.totalTime += this.route.get(j).get(i).getTimeTravel();
                     this.totalCompanyTariff += this.route.get(j).get(i).getCompanyTariff();
                 }
             }
         }
-        
+
         this.totalPrice = (float) (BASE_VALUE / this.totalTime) + this.totalCompanyTariff;
     }
-    
+
     public LinkedList<List<Edge>> getRoute() {
         return route;
     }
@@ -108,5 +110,5 @@ public class Travel implements Serializable {
     public double getTotalTime() {
         return totalTime;
     }
-    
+
 }
